7f28d36b3f00e07335e4cb2764cb1f73c7941667
Rishikesan05 <bsrishi2003@gmail.com>
2026-03-22 12:00:00 +0530

    feature: operator fleet management & driver verification logic

---
+++ lib/modules/operator/operator.dart
@@ -0,0 +1,5 @@
+export 'data/bus_model.dart';
+export 'providers/registration_provider.dart';
+export 'screens/operator_home_screen.dart';
+export 'screens/operator_navigation_hub.dart';
+export 'screens/operator_profile_screen.dart';
