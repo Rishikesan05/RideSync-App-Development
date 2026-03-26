4a9d2d09dc364dfa5ae23e9b8ff5cce920a19eed
Rishikesan05 <bsrishi2003@gmail.com>
2026-03-26 12:00:00 +0530

    feature: passenger interface & location services

---
+++ lib/modules/passenger/screens/live_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ridesync/core/placeholder_screen.dart';
+
+// Real-time tracking map screen
+class LiveScreen extends StatelessWidget {
+  final VoidCallback? onBack;
+  const LiveScreen({super.key, this.onBack});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlaceholderScreen(
+      title: 'Live Tracking',
+      onBack: onBack,
+    );
+  }
+}
+
+
+
+
